31517764ab75cf7881eab67a07ef827d7e590821
choi-jihun <102225856+choi-jihun@users.noreply.github.com>
2023-03-26 18:11:25 +0900

    카메라 얼굴 인식...뭔 소린지 하나도 모르겠다

---
+++ chapter4-1/src/main/java/com/example/chapter4/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.example.chapter4
+
+import android.graphics.PointF
+import android.graphics.RectF
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.transition.TransitionManager
+import android.util.SizeF
+import androidx.core.view.isVisible
+import com.example.chapter4.databinding.ActivityMainBinding
+import com.example.face_recognition.Camera
+import com.example.face_recognition.recognition.FaceAnalyzerListener
+
+class MainActivity : AppCompatActivity(), FaceAnalyzerListener {
+
+    private lateinit var binding: ActivityMainBinding
+    private val camera = Camera(this)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setProgressText("시작하기를 눌러주세요")
+        camera.initCamera(binding.camerLayout, this)
+
+        binding.startDetectButton.setOnClickListener {
+            it.isVisible = false
+            binding.faceOverlayView.reset()
+            camera.startFaceDetect()
+            setProgressText("얼굴을 보여주세요.")
+        }
+
+    }
+
+    override fun detect() {
+        TODO("Not yet implemented")
+    }
+
+    override fun stopDetect() {
+        camera.stopFaceDetect()
+        reset()
+    }
+
+    override fun notDetect() {
+        binding.faceOverlayView.reset()
+    }
+
+    override fun detectProgress(progress: Float, message: String) {
+        setProgressText(message)
+        binding.faceOverlayView.setProgress(progress)
+    }
+
+    override fun faceSize(rectF: RectF, sizeF: SizeF, pointF: PointF) {
+        binding.faceOverlayView.setSize(rectF, sizeF, pointF)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        camera.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    private fun reset() {
+        binding.startDetectButton.isVisible = true
+    }
+
+    private fun setProgressText(text: String) {
+        TransitionManager.beginDelayedTransition(binding.root)
+        binding.progressTextView.text = text
+    }
+}
