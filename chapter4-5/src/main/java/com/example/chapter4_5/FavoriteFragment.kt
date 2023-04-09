77a9fcb859210a9d04966a2e74130276e7f8f4a4
choi-jihun <102225856+choi-jihun@users.noreply.github.com>
2023-04-09 15:33:10 +0900

    이거 왜 안 되는거지

---
+++ chapter4-5/src/main/java/com/example/chapter4_5/FavoriteFragment.kt
@@ -0,0 +1,53 @@
+package com.example.chapter4_5
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
+import com.example.chapter4_5.databinding.FragmentFavoriteBinding
+import com.example.chapter4_5.list.ListAdapter
+
+class FavoriteFragment : Fragment() {
+
+    private var binding: FragmentFavoriteBinding? = null
+    private val adapter = ListAdapter()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return FragmentFavoriteBinding.inflate(inflater, container, false).apply {
+            binding = this
+        }.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.apply {
+            recyclerView.adapter = adapter
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding?.apply {
+            if (Common.favoriteList.isEmpty()) {
+                emptyText.isVisible = true
+                recyclerView.isVisible = false
+            } else {
+                emptyText.isVisible = false
+                recyclerView.isVisible = true
+            }
+        }
+        adapter.submitList(Common.favoriteList.sortedBy { it.dateTime })
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+
+}
